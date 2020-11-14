332e9817c14729febf0f4796329c0604e5b6e89d
rocketmediaasia <64861654+rocketmediaasia@users.noreply.github.com>
2020-11-14 15:28:14 +0700

    Update settings.py

--- plaindjango/settings.py
+++ plaindjango/settings.py
@@ -123,11 +123,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-#  Add configuration for static files storage using whitenoise
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
