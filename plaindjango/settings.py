6ae23b06a0893ad716dbc90cdd81cef9bba64c8d
rocketmediaasia <64861654+rocketmediaasia@users.noreply.github.com>
2020-11-14 14:30:17 +0700

    Update settings.py

--- plaindjango/settings.py
+++ plaindjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'aen#c)1$w+6dpru^y7n2*nio)=d0a*ctp0ndl&3@(=mi8q!2u3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','rocketdjango.herokuapp.com']
 
