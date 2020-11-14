42843bdb108c89c991134ef848dde863829496a6
rocketmediaasia <64861654+rocketmediaasia@users.noreply.github.com>
2020-11-14 16:06:37 +0700

    Update settings.py

--- plaindjango/settings.py
+++ plaindjango/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
-import dj_database_url  
+import os 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -41,7 +40,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
